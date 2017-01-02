9850e41196d483cc6566a35daa86b08ce03e3598
empty6 <empty.bnt@gmail.com>
2017-01-02 20:45:30 +0900

    init
    add all

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof (Camera))]
+
+public class Player : MonoBehaviour {
+	//mouse
+	private Vector3 monsePosition;
+	//mouse (world
+	private Vector3 monsePositionWorld;
+	//angle
+	private float mouseAngle;
+	private bool loaded = true;
+
+
+	// Use this for initialization
+	void Start () {
+		loaded = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(loaded){
+			// Vector3でマウス位置座標を取得する
+			monsePosition = Input.mousePosition;
+			// マウス位置座標をスクリーン座標からワールド座標に変換する
+			monsePositionWorld = Camera.main.ScreenToWorldPoint(monsePosition);
+			monsePositionWorld.z = 0;
+
+			//マウスの向き計算
+			Vector3 targetDir = (monsePositionWorld - transform.position);
+			/*
+			Vector3 arrowDir = (Arrow.transform.position - transform.position).normalized;
+			Vector3.Cross(targetDir, arrowDir);
+			mouseAngle = Mathf.Asin(Vector3.Cross(targetDir, arrowDir).z)*Mathf.Rad2Deg*-1;
+			*/
+			mouseAngle = Mathf.Atan2(targetDir.y, targetDir.x)*Mathf.Rad2Deg;
+
+			//回転
+			this.transform.eulerAngles = new Vector3(0, 0, mouseAngle);
+
+		}
+	}
+}
---
+++ Assets/Scripts/StartButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class StartButton : MonoBehaviour
+{
+
+    /// ボタンをクリックした時の処理
+    public void OnClick()
+    {
+        //Debug.Log("Button click!");
+		// 引数にシーン名を指定する
+		// Build Settings で確認できる sceneBuildIndex を指定しても良い
+		SceneManager.LoadScene("Stage1"); 
+    }
+}
